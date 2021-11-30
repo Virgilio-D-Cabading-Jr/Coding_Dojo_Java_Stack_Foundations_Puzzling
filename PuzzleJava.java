0b54561f87638a7ccf1abf7261e985a9945dad9c
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-30 12:06:37 -0800

    add getTenRolls

--- PuzzleJava.java
+++ PuzzleJava.java
@@ -1,12 +1,34 @@
 import java.util.Random;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ArrayList;
 
 //  /////////////////////////////////////////////////////////////////////
 //  PUZZLE JAVA CLASS
 //  By: Viriglio D. Cabading Jr.    Created at: November 30, 2021 0730
 //  /////////////////////////////////////////////////////////////////////
-    
-// To use methods from the Random library you will need to create an instance of Random
-Random randMachine = new Random();
-// From there you can use any of the methods listed in the documentation. For example:
-randMachine.setSeed(35679); // <--- you won't need to use this method.
 
+// To use methods from the Random library you will need to create an instance of Random
+
+// From there you can use any of the methods listed in the documentation. For example:
+ // <--- you won't need to use this method.
+
+
+public class PuzzleJava {
+
+    // generate and return an array with 10 random numbers between 1 and 20 inclusive.
+    public ArrayList<Integer> getTenRolls() {
+        ArrayList<Integer> intArray = new ArrayList<Integer>();
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HHmmss");
+        int currentTime = Integer.parseInt(simpleDateFormat.format(new Date()));
+
+        Random randMachine = new Random();
+        randMachine.setSeed(currentTime);
+        for (int i=0; i<10; i++) {
+            intArray.add( randMachine.nextInt(20) + 1 );
+        }
+        return intArray;
+    }
+    
+}
--- TestPuzzleJava.java
+++ TestPuzzleJava.java
@@ -1,10 +1,21 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 //  /////////////////////////////////////////////////////////////////////
 //  TEST PUZZLE JAVA APP
 //  By Virgilio D. Cabading Jr.     Created at: November 30, 2021 0730
 //  /////////////////////////////////////////////////////////////////////
 
 public class TestPuzzleJava {
-    public static void main(String[] args) {
-        
-    }
+	public static void main(String[] args) {
+		PuzzleJava generator = new PuzzleJava();
+		ArrayList<Integer> randomRolls = generator.getTenRolls();
+		System.out.println(randomRolls);
+		
+
+    	//..
+		// Write your other test cases here.
+		//..
+	}
 }
+
